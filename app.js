72aac321394473d814a1437488cd81d4aa0610dc
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-08-01 09:40:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ async function getLinhaFromAPI(linha) {
 }
 
 
+const coresMarcadores = {};
+
+
+function definirCorParaValor(valor) {
+  if (!coresMarcadores.hasOwnProperty(valor)) {
+    // Gerar uma cor hexadecimal aleatória
+    const corAleatoria = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    coresMarcadores[valor] = corAleatoria;
+  }
+}
+function getCorMarcador(valor) {
+  // Define a cor do valor (caso ainda não exista)
+  definirCorParaValor(valor);
+
+  return coresMarcadores[valor];
+}
+
 function createMap() {
          const initialZoom = 15;
      map = L.map('map').setView(["-19.923030", "-43.925846"], initialZoom);
@@ -32,36 +49,20 @@ function addMarkersToMap(locations) {
     // Adicionar marcadores para cada localidade e velocidade
     locations.forEach(location => {
         const { LT, LG, NV ,VL} = location;
-        const marker = L.marker([LT, LG]).addTo(map);
-          marker.bindPopup(`${NV} - ${VL}`);
+
+        const corMarcador = getCorMarcador(NV);
+        const marker = L.marker([LT, LG],{ icon: L.divIcon({ 
+        	className: 'custom-icon', html: '<div style="background-color: ' + corMarcador + ';" class="marker-pin"></div>' }) })
+          
+         marker.bindPopup(`${NV} - ${VL}`);
+         marker.addTo(map);
+       
     });
 }
 
-
-function showMap(locations) {
-    const initialZoom = 15;
-     map = L.map('map').setView(["-19.923030", "-43.925846"], initialZoom);
-
-    // Usando o provedor de mapas da OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-       L.marker(["-19.923030", "-43.925846"])
-        .addTo(map)
-        .bindPopup("Você está aqui!");
-
-    // Iterar sobre o array de localidades e adicionar marcadores para cada uma delas
-    locations.forEach(location => {
-        const { LT, LG, NV } = location;
-
-        const marker = L.marker([LT, LG]).addTo(map);
-
-        marker.bindPopup(NV)
-    });
-}
 // Função principal para iniciar o processo
 async function main() {
     try {
-   
         const localizacoes = await getCoordinatesFromAPI("238")
         createMap();
         addMarkersToMap(localizacoes);
